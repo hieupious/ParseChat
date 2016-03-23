35e48baecc1e47850a796e1a281adf72ebabeefe
Hieu Pious <hieupious@ymail.com>
2016-03-23 21:01:43 +0700

    milestone 4

--- ParseChat/ChatViewController.swift
+++ ParseChat/ChatViewController.swift
@@ -10,14 +10,19 @@ import UIKit
 import Parse
 
 class ChatViewController: UIViewController {
-
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    var messages: [PFObject]! = []
     
     @IBOutlet weak var newMessage: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self
+        tableView.dataSource = self
+        getMessage()
+        
+        NSTimer.scheduledTimerWithTimeInterval(5, target: self, selector: #selector(ChatViewController.getMessage), userInfo: nil, repeats: true)
         // Do any additional setup after loading the view.
     }
 
@@ -56,4 +61,45 @@ class ChatViewController: UIViewController {
     }
     */
 
+    func getMessage() {
+        var query = PFQuery(className:"Message_Swift_032016")
+        query.includeKey("user")
+        query.findObjectsInBackgroundWithBlock {
+            (objects: [PFObject]?, error: NSError?) -> Void in
+            
+            if error == nil {
+                // The find succeeded.
+                print("Successfully retrieved \(objects!.count) scores.")
+                // Do something with the found objects
+                if let objects = objects! as? [PFObject] {
+                    self.messages = objects
+                    
+                }
+            } else {
+                // Log details of the failure
+                print("Error: \(error!) \(error!.userInfo)")
+            }
+            self.tableView.reloadData()
+        }
+    }
+    
+    func refreshMessage() {
+    
+    }
+    
+    
+}
+
+extension ChatViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MessCell", forIndexPath: indexPath) as! UITableViewCell
+        var message = self.messages[abs(self.messages.count - indexPath.row - 1)]["body"] as? String ?? self.messages[indexPath.row]["text"] as? String
+        
+        cell.textLabel?.text = message
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.messages.count
+    }
 }
